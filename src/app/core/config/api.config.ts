d4ceafc912137c9898f7254eb19a13947b4070f4
nicovig <nicolas.vigou@gmail.com>
2026-05-22 21:33:08 +0200

    mock

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = 'https://api.kalon-app.fr';
+//export const API_BASE_URL = 'https://api.kalon-app.fr';
 //export const API_BASE_URL = 'https://localhost:7016';
-export const AUTH_MOCK_ENABLED = false;
+export const API_BASE_URL = '';
+export const AUTH_MOCK_ENABLED = true;
