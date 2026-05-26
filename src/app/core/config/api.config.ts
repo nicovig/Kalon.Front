4a8562d4d0e6c207cfb0fb220ad6249b21ff1db1
nicovig <nicolas.vigou@gmail.com>
2026-05-26 14:14:17 +0200

    conf

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,4 +1,4 @@
-//export const API_BASE_URL = 'https://api.kalon-app.fr';
-export const API_BASE_URL = 'https://localhost:7016';
+export const API_BASE_URL = 'https://api.kalon-app.fr';
+//export const API_BASE_URL = 'https://localhost:7016';
 export const AUTH_MOCK_ENABLED = false;
 export const MAX_EMAIL_ATTACHMENTS = 3;
